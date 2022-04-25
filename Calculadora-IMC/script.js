8f6f3f23dfa1954125b0ed83fc1e2399844a4b62
Lauan Amorim <62584060+LauanAmorim@users.noreply.github.com>
2022-04-25 08:13:59 -0300

    Update script.js

--- Calculadora-IMC/script.js
+++ Calculadora-IMC/script.js
@@ -1,18 +1,20 @@
-let resultado = document.getElementById("valor");
-let btnCalcular = document.getElementById("calcular");
-
 function calcularImc(){
-    let inputName = document.getElementById("nome").value;
-    let inputAltura = document.getElementById("altura").value;
-    let inputPeso = document.getElementById("peso").value;
+    let $inputName = document.getElementById("nome").value;
+    let $inputAltura = document.getElementById("altura").value;
+    let $inputPeso = document.getElementById("peso").value;
+    let $resultado = document.getElementById("valor");
     
-    if(!inputName || !inputAltura || !inputPeso){
-        resultado.textContent = "Obrigatorio preencher o fomulário";
+    if(!$inputName || !$inputAltura || !$inputPeso || isNaN(($inputAltura)) || isNaN($inputPeso)){
+        $resultado.textContent = "Obrigatorio preencher o fomulário corretamente";
     }
     else {
-        let result = Number(inputPeso) / Math.pow(Number(inputAltura), 2)
-        resultado.textContent = `Olá ${inputName}, seu IMC é igual a ${result.toFixed(1)}`;
+        $inputPeso = $inputPeso.replace(",", ".")
+        $inputAltura = $inputAltura.replace(",", ".")
+
+        let $result = Number($inputPeso) / Math.pow(Number($inputAltura), 2)
+        $resultado.textContent = `Olá ${$inputName}, seu IMC é igual a ${$result.toFixed(2)}`;
     }
 }
 
-btnCalcular.addEventListener("click", calcularImc)
+let $btnCalcular = document.getElementById("calcular");
+$btnCalcular.addEventListener("click", calcularImc)
